de1d5e3b107aa6d8bd802beba771c455452be254
Le Van Huy <huylv1@itsol.vn>
2017-08-22 14:13:47 +0700

    Fixed for documenting Pageable object in swagger 2.7

---
+++ src/main/java/com/baoviet/mhol/aop/DAOPerformanceAspect.java
@@ -0,0 +1,34 @@
+package com.baoviet.mhol.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Aspect
+@Component
+public class DAOPerformanceAspect {
+
+    Logger logger = LoggerFactory.getLogger(DAOPerformanceAspect.class);
+
+    @Pointcut("within(@org.springframework.stereotype.Repository *)")
+    public void repositoryClassMethods() {};
+
+    @Around("repositoryClassMethods()")
+    public Object measureMethodExecutionTime(ProceedingJoinPoint joinPoint)
+            throws Throwable {
+        long start = System.nanoTime();
+        Object returnValue = joinPoint.proceed();
+        long end = System.nanoTime();
+        String methodName = joinPoint.getSignature().getName();
+        logger.info(
+                "Execution of " + methodName + " took " +
+                        TimeUnit.NANOSECONDS.toMillis(end - start) + " ms");
+        return returnValue;
+    }
+}
--- src/main/java/com/baoviet/mhol/web/controller/PersonController.java
+++ src/main/java/com/baoviet/mhol/web/controller/PersonController.java
@@ -4,17 +4,28 @@ import com.baoviet.mhol.persistence.dao.IPersonDAO;
 import com.baoviet.mhol.persistence.dao.PersonSpecs;
 import com.baoviet.mhol.persistence.model.Person;
 import com.baoviet.mhol.service.PersonService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.Resource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -29,9 +40,22 @@ public class PersonController {
     @Autowired
     private IPersonDAO personDAO;
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", dataType = "integer", paramType = "query",
+                    value = "Results page you want to retrieve (0..N)"),
+            @ApiImplicitParam(name = "size", dataType = "integer", paramType = "query",
+                    value = "Number of records per page."),
+            @ApiImplicitParam(name = "sort", allowMultiple = true, dataType = "string", paramType = "query",
+                    value = "Sorting criteria in the format: property(,asc|desc). " +
+                            "Default sort order is ascending. " +
+                            "Multiple sort criteria are supported.")
+    })
     @RequestMapping(value = "/person")
-    public Page<Person> firstNameStartWith(@RequestParam("firstNameStartWith") String s, Pageable p) {
-        return personDAO.findAll(PersonSpecs.isFirstNameStartWith(s), p);
+    public HttpEntity<PagedResources<Resource<Person>>> firstNameStartWith(@RequestParam("firstNameStartWith") String s,
+                                                                           Pageable pageable, PagedResourcesAssembler<Person> assembler) {
+
+        Page<Person> resources = personDAO.findAll(PersonSpecs.isFirstNameStartWith(s), pageable);
+        return new ResponseEntity<PagedResources<Resource<Person>>>(assembler.toResource(resources), HttpStatus.OK);
     }
 
 }
